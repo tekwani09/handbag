9a3f5a5f3983b341972ee0c9c9c995db29842cd7
lakshya <tekwanilakshya@gmail.com>
2026-08-30 12:58:35 +0530

    fix: remove frontend from PM2, only backend

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,21 +15,6 @@ module.exports = {
       max_memory_restart: '500M',
       instances: 1,
       exec_mode: 'fork'
-    },
-    {
-      name: 'handbag-frontend',
-      script: 'serve -s dist -l 3000',
-      cwd: '/home/ec2-user/handbag-store/frontend',
-      env: {
-        PORT: 3000
-      },
-      error_file: '/home/ec2-user/.pm2/logs/handbag-frontend-error.log',
-      out_file: '/home/ec2-user/.pm2/logs/handbag-frontend-out.log',
-      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
-      watch: false,
-      max_memory_restart: '500M',
-      instances: 1,
-      exec_mode: 'fork'
     }
   ]
 }
